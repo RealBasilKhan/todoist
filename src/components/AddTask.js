b03de1a147459137f15938240689305f5ece3f46
Basil Khan <basilkhan76@gmail.com>
2021-08-26 07:08:39 -0400

    Tasks and Add Project Component Done. Working on AddTask

---
+++ src/components/AddTask.js
@@ -0,0 +1,37 @@
+import react, { userState } from "react";
+import { FaRegListAlt, FaRegCalendarAlt } from "react-icons/fa";
+import moment from "moment";
+import { firebase } from "../firebase";
+import { useSelectedProjectValue } from "../context";
+
+export const AddTask = ({
+  showAddTaskMain = true,
+  showShouldMain = false,
+  showQuickAddTask,
+  setShowQuickAddTask,
+}) => {
+  const [task, setTask] = useState('');
+  const [taskDate, setTaskDate] = useState('')
+  const [project, setProject] = useState('')
+  const [showMain, setShowMain] = useState(shouldShowMain);
+  const [showProjectOverlay, setShowProjectOverlay] = useState(false);
+  const [showTaskDate, setShowTaskDate] = useState(false);
+
+  const { selectedProject} = useSelectedProjectValue();
+
+  const addTask = () => {
+    const projectId = project || selectedProject;
+    let collatedDate = '';
+
+    if (projectId === 'TODAY') {
+      collatedDate = moment().format('DD/MM/YYYY');
+    } else if (projectId === 'NEXT_7') {
+      collatedDate = moment()
+      .add(7, 'days')
+      .format('DD/MM/YYYY');
+    }
+    }
+  }
+
+  return();
+};
